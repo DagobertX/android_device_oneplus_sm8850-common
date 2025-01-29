1f11503aadc0be54117cab4bf6e28fd579960519
dianlujitao <dianlujitao@lineageos.org>
2025-01-29 23:44:41 +0800

    sm8850-common: Switch to AIDL compatibility matrix
    Change-Id: I628ca70be28452c5f347ed21f9ce7632de07a421

--- common.mk
+++ common.mk
@@ -445,7 +445,7 @@ endif
 # VINTF
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/oplus/vintf/device_framework_matrix.xml \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_aidl.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE += device/oneplus/sm8850-common/vintf/framework_manifest.xml
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := \
